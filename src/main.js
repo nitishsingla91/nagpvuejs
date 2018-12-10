4345e44ab58ba0993bafe432d34b86a0d0747744
nitishsingla <nsingla85@gmail.com>
2018-12-10 15:38:46 +0530

    Intial Commit - Create UI Templates

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import App from "./App.vue";
+import VueDevtools from "@vue/devtools";
+import router from "./router";
+import store from "./store";
+
+import '@/assets/main.css';
+
+Vue.config.productionTip = false;
+if (process.env.NODE_ENV === "development") {
+  VueDevtools.connect();
+}
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
---
+++ src/router.js
@@ -0,0 +1,50 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    }, 
+    {
+      path: "/login",
+      name: "login",
+      component: () => import("@/views/Login.vue")
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: () => import("@/views/Register.vue")
+    },
+    {
+      path: "/settings",
+      name: "settings",
+      component: () => import("@/views/Settings.vue")
+    },
+    {
+      path: "/editor",
+      name: "editor_new",
+      component: () => import("@/views/ArticleCreate.vue")
+    },
+    {
+      path: "/editor/:article_slug",
+      name: "editor_edit",
+      component: () => import("@/views/ArticleEdit.vue")
+    },
+    {
+      path: "/article/:article_slug",
+      name: "article",
+      component: () => import("@/views/Article.vue")
+    },
+    {
+      path: "/:username",
+      name: "profile",
+      component: () => import("@/views/Profile.vue")
+    }
+  ]
+});
